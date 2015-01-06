921d6c4fc347d1afd7bd5826f4e6ffe5d206c2ed
taotejacob <jacob.rosch@gmail.com>
2015-01-06 07:07:42 -0500

    v2.5.2
    remove database update

--- FooseMain.py
+++ FooseMain.py
@@ -245,79 +245,8 @@ class Standings(Handler):
 
 class Test(Handler):
 	def get(self):
-		
-		#function to get user names
-		def GetPlayers2(qry):
-			user_list = []
-			for players in qry:
-				user_list.append((str(players.username), str(players.first_last)))
 
-			return user_list
-
-		#create 'first_last' for all users
-		for user in Account.query():
-		    user.first_last = user.first_name + " " + user.last_name[0]
-		    user.put()
-
-
-		#create dictionary between nickname and first_last
-		qry = Account.query().fetch()
-		player_set = GetPlayers2(qry)
-		player_dict = dict(player_set)
-
-		#pull game database
-		gamesDB = db.GqlQuery("SELECT * FROM game_event ORDER BY date DESC")
-
-		#for each row in database replace player id, opp id, player teammate id, with first_last
-		for row in gamesDB:
-			if row.player_id in player_dict.keys():	
-				row.player_id = player_dict[str(row.player_id)]
-
-			if str(row.player_teammate_id) in player_dict.keys():
-				row.player_teammate_id = player_dict[str(row.player_teammate_id)]
-
-			if str(row.opp_id) in player_dict.keys():
-				row.opp_id = str(player_dict[row.opp_id])
-
-			if str(row.game_type) == '2v2':
-				team = teamNamer([row.player_teammate_id])
-				h = []
-
-				for i in team:
-					if i in player_dict.keys():
-						h.append(player_dict[i])
-					else:
-						h.append(i)
-
-				row.player_teammate_id = ''.join(teamNamer(h))
-
-				team = teamNamer([row.opp_id])
-				h = []
-
-				for i in team:
-					if i in player_dict.keys():
-						h.append(player_dict[i])
-					else:
-						h.append(i)
-				row.opp_id = ''.join(teamNamer(h))
-
-			if str(row.game_type) == '1v2':
-				team = teamNamer([row.opp_id])
-				h = []
-
-				for i in team:
-					if i in player_dict.keys():
-						h.append(player_dict[i])
-					else:
-						h.append(i)
-				row.opp_id = ''.join(teamNamer(h))
-
-			row.put()
-
-		self.render('tester.html', 
-			data3 = player_set,
-			# data1 = data1,
-			data2 = player_dict)
+		self.render('tester.html')
 
 
 application = webapp2.WSGIApplication([('/', Welcome),
